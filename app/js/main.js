d65985774dda9b8f335ff6bf601b93d20de786e8
professorLumpen <jeanpaulvsratyi@gmail.com>
2021-12-01 00:32:06 +0300

    added cart and catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
 
+  $('.catalog__btn').on("click", function(){
+    $(this).toggleClass('catalog__btn--active');
+    $('.catalog__list').toggleClass('catalog__list--active');
+  });
+
+  $('.user-icons__link--cart, .shopping-cart__close').on("click", function () {
+    $('.shopping-cart').toggleClass('shopping-cart--active');
+    $('body').toggleClass('body-hidden');
+  });
+
   $('.slider-top').slick({
     infinite: false
   });
